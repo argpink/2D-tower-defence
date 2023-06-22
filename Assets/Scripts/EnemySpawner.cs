1f3100b51c2ba7327572e7aad5e7e11718f62e25
C0dingMast3r77 <denniscullipcross@icloud.com>
2023-06-22 15:10:41 +0100

    Finished video 2 and half way through video 3 (mainly code)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float diffucultyScailingFactor = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Start()
+    {
+        StartWave();
+    }
+
+    private void Update()
+    {
+        if (!isSpawning) return;
+         
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0){
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f; }
+    }
+
+    private void StartWave() {
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave(); }
+
+    private void SpawnEnemy() {
+        GameObject prefabsToSpawn = enemyPrefabs[0];
+        Instantiate(prefabsToSpawn, LevelManager.main.startPoint.position, Quaternion.identity); }
+
+    private int EnemiesPerWave() {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, diffucultyScailingFactor)); }
+}
+
+
