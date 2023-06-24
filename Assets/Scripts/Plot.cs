3e45b1b3659dd7e5c6774d155a38932d0dc1314f
C0dingMast3r77 <denniscullipcross@icloud.com>
2023-06-24 18:57:06 +0100

    Code for the turret placing ('BuildManager.cs' and 'Plot.cs') is almost done but the gun rotation is being an issue. I am on video #6.

---
+++ Assets/Scripts/Plot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plot : MonoBehaviour
+{
+    [Header("Referances")]
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private Color hoverColor; 
+
+    private GameObject tower;
+    private Color startColor;
+
+    private void Start()
+    {startColor = sr.color;}
+
+    private void OnMouseEnter()
+    { sr.color = hoverColor; }
+
+    private void OnMouseExit()
+    {sr.color = startColor; }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Build tower here;" + name);
+    }
+}
