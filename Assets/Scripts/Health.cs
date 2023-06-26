1283f04a9708edb5f8ba3b7d66b2efb97f9679b4
C0dingMast3r77 <denniscullipcross@icloud.com>
2023-06-26 18:25:11 +0100

    Currency and amountToPay for items completed. Video 7 complete.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int currencyWorth = 50;
 
     private bool isDestroyed = false;
     public void TakeDamage(int dmg){
@@ -13,6 +14,7 @@ public class Health : MonoBehaviour
         
             if (hitPoints <= 0 && !isDestroyed) {
                 EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyWorth);
             isDestroyed = true;
              Destroy(gameObject); }
         }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager main;
 
-    public Transform startPoint;  
-    public Transform[] path; 
+    public Transform startPoint;
+    public Transform[] path;
 
+    public int currency;
     private void Awake()
+    { main = this; }
+
+    private void Start()
+    { currency = 100; }
+
+    public void IncreaseCurrency(int amountToPay)
+    { currency += amountToPay; }
+
+    public bool SpendCurrency(int amountToPay)
     {
-        main = this;
+        if (amountToPay <= currency)
+        {
+            currency -= amountToPay;
+            return true;
+            // BUY ITEM
+        }
+        else
+        {
+            Debug.Log("You do not have enough money to purchase this item.");
+            return false;
+        }
+
     }
 }
